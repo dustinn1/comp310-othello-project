8d392346e2fff44fea89a068ff3a0d4562b80aa0
Dustin Nguyen <dnguyen13@luc.edu>
2020-11-14 23:32:52 -0600

    Update tree struct and add node delete function

--- tree.c
+++ tree.c
@@ -4,12 +4,22 @@
 #include "board.h"
 
 point_t* node_init(node_t *node, board_t *board) {
+	node->depth = 1;
 	node->board = board;
 	node->children = malloc(sizeof(node_t));
+	node->numOfChildren = 0;
+	node->player = 'C';
+	node->piecesFlipped = 0;
 }
 
 point_t* node_add(node_t *node, node_t *parent) {
+	node->depth = parent->depth+1;
 	board_copy(node->board, parent->board);
 	parent->numOfChildren += 1;
 	parent->children = realloc(parent->children, parent->numOfChildren * sizeof(node_t));
+}
+
+void node_delete(node_t *node) {
+	node = calloc(1, sizeof(node_t));
+	free(node);
 }
--- tree.h
+++ tree.h
@@ -4,13 +4,18 @@
 #include "board.h"
 
 typedef struct _node_t {
+	int depth;
 	board_t* board;
-	int numOfChildren;
 	struct _node_t* children;
+	int numOfChildren;
+	char player;
+	int piecesFlipped;
 } node_t;
 
 extern point_t* node_init(node_t *node, board_t *board);
 
 extern point_t* node_add(node_t *node, node_t *parent);
+
+extern void node_delete(node_t *node);
 
 #endif
