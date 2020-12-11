6e2f1d7ea01a7a6d3f0ccb70eca613901c15d600
Dustin Nguyen <dnguyen13@luc.edu>
2020-12-10 19:16:33 -0600

    Update tree tests

--- tree_tests.cc
+++ tree_tests.cc
@@ -2,14 +2,13 @@
 
 #include "tree.hh"
 
-/*
 TEST(TreeTest, Create) {
 	board_t* board = board_init();
 	board_add_piece(board, 'P', 2, 3);
 
 	node_t* root = node_init(board);
-    tree_create(root);
-}*/
+    tree_create(root, 3);
+}
 
 TEST(TreeTest, GetMax) {
 	board_t* board = board_init();
@@ -20,7 +19,7 @@ TEST(TreeTest, GetMax) {
 	node_t* max = tree_get_max(root, 5);
 	ASSERT_EQ(2, max->firstPieceAdded.x);
 	ASSERT_EQ(2, max->firstPieceAdded.y);
-	ASSERT_EQ(13, max->value);
+	ASSERT_EQ(18, max->value);
 }
 
 int main(int argc, char **argv) {
