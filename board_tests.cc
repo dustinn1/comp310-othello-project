6753dfddbbda49ffd6488c0ca11fec2834675f74
Dustin Nguyen <dnguyen13@luc.edu>
2020-12-10 19:26:34 -0600

    Fix board tests

--- board_tests.cc
+++ board_tests.cc
@@ -3,79 +3,67 @@
 #include "board.hh"
 
 TEST(BoardTest, Initialization) {
-	static board_t board;
+	board_t* board = board_init();
 
-	board_init(&board);
-
-	ASSERT_TRUE(board.pieces[3][3] != NULL);
-	ASSERT_TRUE(board.pieces[3][4] != NULL);
+	ASSERT_TRUE(board->pieces[3][3] != NULL);
+	ASSERT_TRUE(board->pieces[3][4] != NULL);
 }
 
 TEST(BoardTest, Add) {
-	static board_t board;
+	board_t* board = board_init();
 
-	board_init(&board);
+	board_add_piece(board, 'P', 2, 3);
+	board_add_piece(board, 'P', 4, 5);
 
-	board_add_piece(&board, 'P', 2, 3);
-	board_add_piece(&board, 'P', 4, 5);
-
-	ASSERT_TRUE(board.pieces[3][2] != NULL);
-	ASSERT_TRUE(board.pieces[5][4] != NULL);
-	ASSERT_EQ('P', board.pieces[5][4]->color);
+	ASSERT_TRUE(board->pieces[3][2] != NULL);
+	ASSERT_TRUE(board->pieces[5][4] != NULL);
+	ASSERT_EQ('P', board->pieces[5][4]->color);
 }
 
 TEST(BoardTest, Flip) {
-	static board_t board;
+	board_t* board = board_init();
 
-	board_init(&board);
+	ASSERT_EQ('C', board->pieces[3][3]->color);
+	ASSERT_EQ('P', board->pieces[3][4]->color);
 
-	ASSERT_EQ('C', board.pieces[3][3]->color);
-	ASSERT_EQ('P', board.pieces[3][4]->color);
+	board_add_piece(board, 'P', 2, 3);
+	board_add_piece(board, 'C', 4, 2);
 
-	board_add_piece(&board, 'P', 2, 3);
-	board_add_piece(&board, 'C', 4, 2);
-
-	ASSERT_EQ('P', board.pieces[3][3]->color);
-	ASSERT_EQ('C', board.pieces[3][4]->color);
+	ASSERT_EQ('P', board->pieces[3][3]->color);
+	ASSERT_EQ('C', board->pieces[3][4]->color);
 
 }
 
 TEST(BoardTest, FlipDiagonal) {
-	static board_t board;
+	board_t* board = board_init();
+	
+	board->pieces[0][0] = piece_init('C', 0, 0);
+	board->pieces[1][1] = piece_init('P', 1, 1);
 
-	board_init(&board);
+	board->pieces[0][6] = piece_init('P', 6, 0);
+	board->pieces[1][5] = piece_init('C', 5, 1);
 	
-	board.pieces[0][0] = piece_init('C', 0, 0);
-	board.pieces[1][1] = piece_init('P', 1, 1);
+	board->pieces[6][0] = piece_init('P', 0, 6);
+	board->pieces[5][1] = piece_init('C', 1, 5);
+	
+	board->pieces[6][6] = piece_init('C', 6, 6);
+	board->pieces[5][5] = piece_init('P', 5, 5);
+	
+	board_add_piece(board, 'C', 2, 2);
+	board_add_piece(board, 'P', 4, 2);
+	board_add_piece(board, 'P', 2, 4);
+	board_add_piece(board, 'C', 4, 4);
 
-	board.pieces[0][6] = piece_init('P', 6, 0);
-	board.pieces[1][5] = piece_init('C', 5, 1);
-	
-	board.pieces[6][0] = piece_init('P', 0, 6);
-	board.pieces[5][1] = piece_init('C', 1, 5);
-	
-	board.pieces[6][6] = piece_init('C', 6, 6);
-	board.pieces[5][5] = piece_init('P', 5, 5);
-	
-	board_add_piece(&board, 'C', 2, 2);
-	board_add_piece(&board, 'P', 4, 2);
-	board_add_piece(&board, 'P', 2, 4);
-	board_add_piece(&board, 'C', 4, 4);
-
-	ASSERT_EQ('C', board.pieces[1][1]->color);
-	ASSERT_EQ('P', board.pieces[1][5]->color);
-	ASSERT_EQ('P', board.pieces[5][1]->color);
-	ASSERT_EQ('C', board.pieces[5][5]->color);
+	ASSERT_EQ('C', board->pieces[1][1]->color);
+	ASSERT_EQ('P', board->pieces[1][5]->color);
+	ASSERT_EQ('P', board->pieces[5][1]->color);
+	ASSERT_EQ('C', board->pieces[5][5]->color);
 }
 
 TEST(BoardTest, Full) {
-	static board_t board;
+	board_t* board = board_init();
 
-	board_init(&board);
-
-	ASSERT_FALSE(board_is_full(&board));
-
-	// implement test for if full -- can only think of adding 60 pieces at the momement
+	ASSERT_FALSE(board_is_full(board));
 }
 
 int main(int argc, char **argv) {
